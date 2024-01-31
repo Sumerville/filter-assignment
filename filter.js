1afc37303b36caf3a35b67d8144ff69cd437cf9c
Sumerville <uwaaustin10@gmail.com>
2024-01-31 00:33:31 -0800

    initial commit

---
+++ filter.js
@@ -0,0 +1,648 @@
+let products ={
+    data:[ 
+        {
+        name:"classic",
+        category:"menfootwear",
+        price:"1000",
+        image:"images/menshoe12.jpg"
+    },
+    {
+        name:"classic",
+        category:"menfootwear",
+        price:"1000",
+        image:"images/menshoe2.jpg"
+    },
+
+
+    {
+        name:"classic",
+        category:"menfootwear",
+        price:"1000",
+        image:"images/menshoe3.jpg"
+    },
+
+    {
+        name:"classic",
+        category:"menfootwear",
+        price:"1000",
+        image:"images/menshoe4.jpg"
+    },
+
+
+    {
+        name:"classic",
+        category:"menfootwear",
+        price:"1000",
+        image:"images/menshoe5.jpg"
+    },
+
+    {
+        name:"classic",
+        category:"menfootwear",
+        price:"1000",
+        image:"images/menshoe6.jpg"
+    },
+
+
+    {
+        name:"alto",
+        category:"menfootwear",
+        price:"1200",
+        image:"images/shoe7.jpg"
+    },
+    {
+        name:"alto",
+        category:"menfootwear",
+        price:"1200",
+        image:"images/shoe12.jpg"
+    },
+
+
+    {
+        name:"alto",
+        category:"menfootwear",
+        price:"1200",
+        image:"images/shoew5.jpg"
+    },
+
+    {
+        name:"alto",
+        category:"menfootwear",
+        price:"1200",
+        image:"images/shoeb7.jpg"
+    },
+
+
+    {
+        name:"alto",
+        category:"menfootwear",
+        price:"1200",
+        image:"images/shoeb8.jpg"
+    },
+
+    {
+        name:"alto",
+        category:"menfootwear",
+        price:"1200",
+        image:"images/shoe20.jpg"
+    },
+
+    {
+        name:"breathe",
+        category:"menfootwear",
+        price:"1500",
+        image:"images/shoe5.jpg"
+    },
+    {
+        name:"breathe",
+        category:"menfootwear",
+        price:"1500",
+        image:"images/shoe11.jpg"
+    },
+
+
+    {
+        name:"breathe",
+        category:"menfootwear",
+        price:"1500",
+        image:"images/shoe11a.webp"
+    },
+
+    {
+        name:"breathe",
+        category:"menfootwear",
+        price:"1500",
+        image:"images/shoegreen1.jpg"
+    },
+
+
+    {
+        name:"breathe",
+        category:"menfootwear",
+        price:"1500",
+        image:"images/shoered3.jpg"
+    },
+
+    {
+        name:"breathe",
+        category:"menfootwear",
+        price:"1500",
+        image:"images/shoew4.jpg"
+    },
+
+    {
+        name:"classic",
+        category:"womenfootwear",
+        price:"1000",
+        image:"images/women2.jpg"
+    },
+    {
+        name:"classic",
+        category:"womenfootwear",
+        price:"1000",
+        image:"images/women4.jpg"
+    },
+
+
+    {
+        name:"classic",
+        category:"womenfootwear",
+        price:"1000",
+        image:"images/women5.jpg"
+    },
+
+    {
+        name:"classic",
+        category:"womenfootwear",
+        price:"1000",
+        image:"images/women7.jpg"
+    },
+
+
+    {
+        name:"classic",
+        category:"womenfootwear",
+        price:"1000",
+        image:"images/women8.jpg"
+    },
+
+    {
+        name:"classic",
+        category:"womenfootwear",
+        price:"1000",
+        image:"images/women9.jpg"
+    },
+
+
+    {
+        name:"alto",
+        category:"womenfootwear",
+        price:"1200",
+        image:"images/women6.jpg"
+    },
+    {
+        name:"alto",
+        category:"womenfootwear",
+        price:"1200",
+        image:"images/women10.jpg"
+    },
+
+
+    {
+        name:"alto",
+        category:"womenfootwear",
+        price:"1200",
+        image:"images/women11.jpg"
+    },
+
+    {
+        name:"alto",
+        category:"womenfootwear",
+        price:"1200",
+        image:"images/women13.jpg"
+    },
+
+
+    {
+        name:"alto",
+        category:"womenfootwear",
+        price:"1200",
+        image:"images/women14.jpg"
+    },
+
+    {
+        name:"alto",
+        category:"womenfootwear",
+        price:"1200",
+        image:"images/women15.jpg"
+    },
+
+    {
+        name:"breathe",
+        category:"womenfootwear",
+        price:"1500",
+        image:"images/women12.jpg"
+    },
+    {
+        name:"breathe",
+        category:"womenfootwear",
+        price:"1500",
+        image:"images/women16.jpg"
+    },
+
+
+    {
+        name:"breathe",
+        category:"womenfootwear",
+        price:"1500",
+        image:"images/women17.jpg"
+    },
+
+    {
+        name:"breathe",
+        category:"womenfootwear",
+        price:"1500",
+        image:"images/women18.jpg"
+    },
+
+
+    {
+        name:"breathe",
+        category:"womenfootwear",
+        price:"1500",
+        image:"images/women19.jpg"
+    },
+
+    {
+        name:"breathe",
+        category:"womenfootwear",
+        price:"1500",
+        image:"images/women20.jpg"
+    },
+
+
+    {
+        name:"classic",
+        category:"childrenfootwear",
+        price:"1000",
+        image:"images/children3.jpg"
+    },
+    {
+        name:"classic",
+        category:"childrenfootwear",
+        price:"1000",
+        image:"images/children4.jpg"
+    },
+
+
+    {
+        name:"classic",
+        category:"childrenfootwear",
+        price:"1000",
+        image:"images/children5.jpg"
+    },
+
+    {
+        name:"classic",
+        category:"childrenfootwear",
+        price:"1000",
+        image:"images/children6.jpg"
+    },
+
+
+    {
+        name:"classic",
+        category:"childrenfootwear",
+        price:"1000",
+        image:"images/children7.jpg"
+    },
+
+    {
+        name:"classic",
+        category:"childrenfootwear",
+        price:"1000",
+        image:"images/children8.jpg"
+    },
+
+
+    {
+        name:"alto",
+        category:"childrenfootwear",
+        price:"1200",
+        image:"images/children9.jpg"
+    },
+    {
+        name:"alto",
+        category:"childrenfootwear",
+        price:"1200",
+        image:"images/children10.jpg"
+    },
+
+
+    {
+        name:"alto",
+        category:"cildrenfootwear",
+        price:"1200",
+        image:"images/children11.jpg"
+    },
+
+    {
+        name:"alto",
+        category:"childrenfootwear",
+        price:"1200",
+        image:"images/children12.jpg"
+    },
+
+
+    {
+        name:"alto",
+        category:"childrenfootwear",
+        price:"1200",
+        image:"images/children13.jpg"
+    },
+
+    {
+        name:"alto",
+        category:"childrenfootwear",
+        price:"1200",
+        image:"images/children14.jpg"
+    },
+
+    {
+        name:"breathe",
+        category:"childrenfootwear",
+        price:"1500",
+        image:"images/children15.jpg"
+    },
+    {
+        name:"breathe",
+        category:"childrenfootwear",
+        price:"1500",
+        image:"images/children16.jpg"
+    },
+
+
+    {
+        name:"breathe",
+        category:"childrenfootwear",
+        price:"1500",
+        image:"images/children17.jpg"
+    },
+
+    {
+        name:"breathe",
+        category:"childrenfootwear",
+        price:"1500",
+        image:"images/children18.jpg"
+    },
+
+
+    {
+        name:"breathe",
+        category:"childrenfootwear",
+        price:"1500",
+        image:"images/children19.jpg"
+    },
+
+    {
+        name:"breathe",
+        category:"childrenfootwear",
+        price:"1500",
+        image:"images/children20.jpg"
+    },
+
+
+    {
+        name:"M-watch",
+        category:"watch",
+        price:"1000",
+        image:"images/mwatch2.jpg"
+    },
+    {
+        name:"M-watch",
+        category:"watch",
+        price:"1000",
+        image:"images/mwatch4.jpg"
+    },
+
+
+    {
+        name:"M-watch",
+        category:"watch",
+        price:"1000",
+        image:"images/mwatch5.jpg"
+    },
+
+    {
+        name:"M-watch",
+        category:"watch",
+        price:"1000",
+        image:"images/mwatch6.jpg"
+    },
+
+
+    {
+        name:"M-watch",
+        category:"watch",
+        price:"1000",
+        image:"images/mwatch7.jpg"
+    },
+
+    {
+        name:"M-watch",
+        category:"watch",
+        price:"1000",
+        image:"images/mwatch8.jpg"
+    },
+
+
+
+
+    {
+        name:"W-watch",
+        category:"watch",
+        price:"1200",
+        image:"images/wowatch1.jpg"
+    },
+    {
+        name:"W-watch",
+        category:"watch",
+        price:"1200",
+        image:"images/wowatch2.jpg"
+    },
+
+
+    {
+        name:"W-watch",
+        category:"watch",
+        price:"1200",
+        image:"images/wowatch3.jpg"
+    },
+
+    {
+        name:"W-watch",
+        category:"watch",
+        price:"1200",
+        image:"images/wowatch4.jpg"
+    },
+
+
+    {
+        name:"W-watch",
+        category:"watch",
+        price:"1200",
+        image:"images/wowatch5.jpg"
+    },
+
+    {
+        name:"W-watch",
+        category:"watch",
+        price:"1200",
+        image:"images/wowatch6.jpg"
+    },
+
+
+
+    {
+        name:"C-watch",
+        category:"watch",
+        price:"1500",
+        image:"images/kids1.jpg"
+    },
+    {
+        name:"C-watch",
+        category:"watch",
+        price:"1500",
+        image:"images/kids3.jpg"
+    },
+
+
+    {
+        name:"C-watch",
+        category:"watch",
+        price:"1500",
+        image:"images/kids4.jpg"
+    },
+
+    {
+        name:"C-watch",
+        category:"watch",
+        price:"1500",
+        image:"images/kids5.jpg"
+    },
+
+
+    {
+        name:"C-watch",
+        category:"watch",
+        price:"1500",
+        image:"images/kids6.jpg"
+    },
+
+    {
+        name:"C-watch",
+        category:"watch",
+        price:"1500",
+        image:"images/kids7.jpg"
+    },
+],
+
+};
+console.log(products)
+
+for(let i of products.data){
+    //create card
+    let card = document.createElement("div");
+    //card should have category and stay hidden initially
+    card.classList.add("card", i.category, "hide");
+    // image div
+    let imgContainer = document.createElement("div");
+    imgContainer.classList.add("image-container");
+    //image tag
+    let image =document.createElement("img");
+    image.setAttribute("src", i.image);
+    imgContainer.appendChild(image);
+    card.appendChild(imgContainer);
+
+    //container
+    let container = document.createElement("div");
+    container.classList.add("container");
+    //product name
+    let productName = document.createElement("h5");
+    productName.classList.add("product-name");
+    productName.innerText= i.name.toUpperCase();
+    container.appendChild(productName);
+    card.appendChild(container);
+    //price
+    let price = document.createElement("h6");
+    price.innerText = "N" + i.price;
+    container.appendChild(price);
+
+
+    document.getElementById("products").appendChild(card);
+}
+
+//parameter passed from button
+
+function filterProduct(value){
+let buttons = document.querySelectorAll(".button-value");
+buttons.forEach((button)=>{
+    if(value.toUpperCase()==button.innerText.toUpperCase()){
+        button.classList.add("active");
+    }else{
+        button.classList.remove("active");
+    }
+let elements = document.querySelectorAll(".card");
+elements.forEach((elemennt)=>{
+   if(value=="all"){
+    elemennt.classList.remove("hide");
+   }else{
+    if(elemennt.classList.contains(value)){
+        elemennt.classList.remove("hide");
+    }else{
+        elemennt.classList.add("hide");
+    }
+   }
+})
+});
+};
+// const filteredProducts = products.filter(function(){
+//     if(products.data.category=="watch"){
+//         return true
+//     }
+// });
+// console.log(filteredProducts)
+// watchBtn = document.getElementById("watch")
+// const filtereddCompanies = products.data.filter((company)=> company.category=="watch")
+//  console.log(filtereddCompanies);
+ 
+
+function filterResult(){
+
+}
+
+
+document.getElementById("search").addEventListener("click", ()=>{
+    let searchInput = document.getElementById("search-input").value;
+    let elements = document.querySelectorAll(".product-name");
+    let cards = document.querySelectorAll(".card");
+    //console.log(searchInput)
+    elements.forEach((elemennt, index)=>{
+        if(elemennt.innerText.includes(searchInput.toUpperCase())){
+            cards[index].classList.remove("hide");
+        }else{
+            cards[index].classList.add("hide");
+        }
+    })
+})
+
+
+
+
+// searchBtn = document.getElementById("search");
+
+// searchBtn.addEventListener("click", ()=>{
+//     let searchInput = document.getElementById("search-input").value;
+//     let elements = document.querySelectorAll(".category");
+//     let cards = document.querySelectorAll(".card");
+
+// elements.forEach((elemennt, index )=>{
+// if(elemennt.innerText.includes(searchInput.toUpperCase())){
+//     cards[i].classList.remove("hide");
+// }else{
+//     cards[i].classList.add("hide");
+// }
+// });
+
+//     console.log(searchInput);
+// });
+   
+
+//initially display all products
+ window.onload = ()=>{
+
+filterProduct("all");
+ };
+
+
+
